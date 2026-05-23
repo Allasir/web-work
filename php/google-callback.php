2fdc33d17ba297a95b8486fb8f66f7cc54450493
Allasir <igorziis@inbox.lv>
2026-05-23 19:41:21 +0300

    Update google-callback.php

--- php/google-callback.php
+++ php/google-callback.php
@@ -5,7 +5,7 @@ require_once 'vendor/autoload.php';
 // 1. Configure Google Client
 $client = new Google_Client();
 $client->setClientId('1097292719652-plk0lmgoscknjhd63ae79dv2r94c5d07.apps.googleusercontent.com');
-$client->setClientSecret('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');
+$client->setClientSecret('');
 $client->setRedirectUri('https://web-work.gamer.gd/php/google-callback.php');
 
 // 2. Authenticate Code from Google
